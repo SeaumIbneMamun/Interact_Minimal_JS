13fee35f1d24169a482c5b770f7c5eb26bdea1a0
Md. Seaum Ibne Mamun <94161634+SeaumIbneMamun@users.noreply.github.com>
2023-11-16 09:11:09 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+document.getElementById('btn').addEventListener('click', function(){
+    document.body.style.backgroundColor = '#00ff97'
+    document.body.style.color = '#020062'
+    
+})
---
+++ index.js
@@ -0,0 +1,68 @@
+// var name = 'Maria';
+
+// function add (x , y){
+//     const total = x + y;
+//     console.log(tota);
+//     return total
+// }
+
+// console.log(total);
+// add(32 , 64)
+
+
+// const person = {
+//     firstName: "John",
+//     lastName: "Doe",
+//     id:5656,
+//     fullName: function(){
+//         return this.firstName + " " + this.lastName;
+//     }
+// };
+
+// console.log(person.fullName());
+
+
+// const person = {
+//     firstName: "John",
+//     lastName: "Doe",
+//     id:5656,
+//     fullName: function(){
+//         return this
+//     }
+// };
+
+// console.log(person.fullName());
+
+
+
+const age = 22;
+const isRomantic = true;
+const name = 'Ayesa Alamin';
+
+// primitive
+// string
+// number
+// bigint
+// boolean
+// undefined
+// symbol
+// null
+
+// let a = 'Hello';
+// let b = a;
+// a = 'gell'
+// console.log(a , b); 
+
+// non-primitive
+// object
+
+
+const x = {
+    name: 'Sakib Al Hassan',
+    job: 'Cricketer'
+}
+
+const y = x
+x.job = 'Opening Showroom';
+
+console.log(x , y);
